cbe260926139fe104fd1363d45ec402529bc465e
veronica365 <naruth10@gmail.com>
2023-07-16 08:29:54 +0300

    Add redux logger

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import motorReducer from '../reducers/motorcycleReducer';
 import usersSlice from '../reducers/usersSlice';
 
+const weAreNotLive = process.env.NODE_ENV !== 'production';
 const store = configureStore({
   reducer: {
     motorcycles: motorReducer,
     users: usersSlice,
   },
+  middleware: (getDefaultMiddleware) => {
+    if (weAreNotLive) return getDefaultMiddleware().concat(logger);
+    return getDefaultMiddleware();
+  },
+  devTools: weAreNotLive,
 });
 
 export default store;
