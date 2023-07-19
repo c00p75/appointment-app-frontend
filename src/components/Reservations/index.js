41af3dd509ec0f4b40a1158008cb99e0d669c239
veronica365 <naruth10@gmail.com>
2023-07-19 16:07:39 +0300

    Add reservations from the backend

--- src/components/Reservations/index.js
+++ src/components/Reservations/index.js
@@ -1,27 +1,43 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getMyReservations } from '../../redux/actions/reservationActions';
 
 export default function Reservations() {
   const dispatch = useDispatch();
+  const { reservations } = useSelector((store) => store.reservations);
   useEffect(() => {
     dispatch(getMyReservations());
   }, [dispatch]);
   return (
     <div className="px-3 py-5">
-      <ul className="list-group">
-        <li className="list-group-item disabled d-flex">
-          <div>
-            <span>Reservation name</span>
-          </div>
-          <div className="px-3">
-            <span>Date</span>
-          </div>
-          <div>
-            <span>City</span>
-          </div>
-        </li>
-      </ul>
+      <h4>My Reservations</h4>
+      {reservations.length === 0 && (
+        <div className="py-5 text-center w-100">Your reservations for motorcycles shall appear here</div>
+      )}
+      {reservations.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Reserve No</th>
+              <th scope="col">Motorcycle</th>
+              <th scope="col">City</th>
+              <th scope="col">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reservations.map((reservation, index) => (
+              <tr key={reservation.id}>
+                <th scope="row">{index + 1}</th>
+                <th>{reservation.id}</th>
+                <td>{reservation.motorcycle.model}</td>
+                <td>{reservation.city}</td>
+                <td>{reservation.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
