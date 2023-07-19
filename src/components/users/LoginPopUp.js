b3f279b9f02d9b19a2d552e8809f75ace4e0b022
mossa <martin.ossa.8@gmail.com>
2023-07-19 14:20:02 +0200

    Fix login popup closing issue

--- src/components/users/LoginPopUp.js
+++ src/components/users/LoginPopUp.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Login from './Login';
 
 function LoginPopup({ handleClose }) {
+  useEffect(() => {
+    function cleanup() {
+      handleClose();
+    }
+
+    return cleanup;
+  }, [handleClose]);
+
   return (
     <div className="d-flex justify-content-center align-items-center h-100 w-100 position-fixed top-50 start-50 translate-middle" style={{ zIndex: 9998, backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
       <div className="w-50 h-30 login-popup p-4 bg-light" style={{ zIndex: 9999, borderRadius: '1rem' }}>
