43de468ed17a9f1bfcb8622f4efa206acb1d8853
c00p75_ <georgecoopmsapenda@gmail.com>
2023-07-20 16:45:10 +0200

    Add delete route

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 import Motorcycles from './Motorcycles';
 import Register from './users/Register';
 import Login from './users/Login';
@@ -9,6 +8,7 @@ import Motorcycle from './motorcycles/Motorcycle';
 import MotorcycleForm from './motorcycleForm/MotorcycleForm';
 import Reservations from './Reservations';
 import ReserveForm from './reservationForm/ReservationForm';
+import MotorcycleDeleteForm from './motorcycleForm/MotorcycleDeleteForm';
 
 function App() {
   return (
@@ -16,8 +16,9 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route path="/motorcycles" element={<Motorcycles />} />
         <Route path="/motorcycles/new" element={<MotorcycleForm />} />
+        <Route path="/motorcycles/delete" element={<MotorcycleDeleteForm />} />
         <Route path="/reservations" element={<Reservations />} />
-        <Route path="/reserve" element={<ReserveForm />} />
+        <Route path="/reservations/new" element={<ReserveForm />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/motorcycles/:id" element={<Motorcycle />} />
